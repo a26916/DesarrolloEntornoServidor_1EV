11b4a54a8a94c84b21d43496529287a23c45bec3
a26916 <a26916@svalero.com>
2025-11-25 16:05:56 +0100

    Creada la BBDD

--- CreateDBMySQL.sql
+++ CreateDBMySQL.sql
@@ -1,3 +1,23 @@
+CREATE TABLE Vehiculo (
+    VehiculoID INT PRIMARY KEY IDENTITY(1,1),
+    Marca VARCHAR(100) NOT NULL,
+    Modelo VARCHAR(100) NOT NULL,
+    Año INT NOT NULL,
+    Precio DECIMAL(10, 2) NOT NULL,
+    EsImportado BIT NOT NULL
+);
+
+CREATE TABLE Coche (
+    VehiculoID INT PRIMARY KEY, 
+    
+    TipoCarroceria VARCHAR(100) NOT NULL,
+    
+    AcabadoID INT NOT NULL,
+    
+    FOREIGN KEY (VehiculoID) REFERENCES Vehiculo(VehiculoID),
+    FOREIGN KEY (AcabadoID) REFERENCES Acabado(AcabadoID)
+);
+
 CREATE TABLE Motor (
     MotorID INT PRIMARY KEY IDENTITY(1,1),
     PotenciaCV INT NOT NULL,
@@ -10,4 +30,16 @@ CREATE TABLE Rueda (
     DiametroPulgadas INT NOT NULL,
     AnchoMm INT NOT NULL,
     Perfil INT NULL
+);
+
+CREATE TABLE Acabado (
+    AcabadoID INT PRIMARY KEY IDENTITY(1,1),
+    NombreAcabado VARCHAR(100) NOT NULL,
+    MultiplicadorPrecio DECIMAL(3,2) NOT NULL,
+    
+    MotorID INT NOT NULL,
+    RuedaID INT NOT NULL,
+    
+    FOREIGN KEY (MotorID) REFERENCES Motor(MotorID),
+    FOREIGN KEY (RuedaID) REFERENCES Rueda(RuedaID)
 );
